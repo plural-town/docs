cdd0dd68cc4fa76a8de431aa819aba8275503e32
Flyyn <_@rst.vn>
2023-01-17 10:10:02 -0500

    Replace NX template with initial PluralTown content

--- apps/documentation/pages/_app.tsx
+++ apps/documentation/pages/_app.tsx
@@ -1,6 +1,8 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import { ChakraProvider, extendTheme, theme } from "@chakra-ui/react";
+import { AppProps } from "next/app";
+import Head from "next/head";
+
+const appTheme = extendTheme({}, theme);
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,8 +10,10 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to documentation!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
+      <main>
+        <ChakraProvider theme={appTheme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
       </main>
     </>
   );
